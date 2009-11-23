8584a755c1dbc8a19cb444fab6778682d78b6cd3
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2009-11-24 02:19:46 +0900

    git-debimport kexec-tools_2.0.1.orig.tar.gz

---
+++ kexec/arch/sh/kexec-sh.h
@@ -0,0 +1,25 @@
+#ifndef KEXEC_SH_H
+#define KEXEC_SH_H
+
+#define COMMAND_LINE_SIZE 2048
+
+int zImage_sh_probe(const char *buf, off_t len);
+int zImage_sh_load(int argc, char **argv, const char *buf, off_t len,
+	struct kexec_info *info);
+void zImage_sh_usage(void);
+
+int elf_sh_probe(const char *buf, off_t len);
+int elf_sh_load(int argc, char **argv, const char *buf, off_t len,
+	struct kexec_info *info);
+void elf_sh_usage(void);
+
+int netbsd_sh_probe(const char *buf, off_t len);
+int netbsd_sh_load(int argc, char **argv, const char *buf, off_t len,
+	struct kexec_info *info);
+void netbsd_sh_usage(void);
+
+char *get_append(void);
+void kexec_sh_setup_zero_page(char *zero_page_buf, int zero_page_size,
+			      char *cmd_line);
+
+#endif /* KEXEC_SH_H */
